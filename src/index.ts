dedc92ffba148410df481ec7948c3bf6b209623a
bpbot33 <brunopereyra.dev@gmail.com>
2022-11-24 20:32:04 -0300

    first commit - one route

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import characterRoutes from "./routes/characterRoutes";
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get("/ping", (_req, res) => {
+  console.log("someone pinged here!!");
+  res.send("pong");
+});
+
+app.use("/characters", characterRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
